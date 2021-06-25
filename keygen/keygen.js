010f1e4555f0be3ee5ac949cc0d0e958a1a9f0cf
50bits <85784459+50bits@users.noreply.github.com>
2021-06-25 23:18:03 +0200

    timelock puzzle

--- keygen/keygen.js
+++ keygen/keygen.js
@@ -2,27 +2,24 @@ let generatedVaultKeys = {};
 
 // step 1
 function generateVaultKeys() {
-    let years = ["2021", "2022"];
-    let months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+    let years = ["2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030", "2031"];
     years.forEach(function (y) {
-        months.forEach(function (m) {
-            generatedVaultKeys[m + "/" + y] = {};
-            window.crypto.subtle.generateKey(
-                {
-                    name: "RSA-OAEP",
-                    modulusLength: 2048,
-                    publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-                    hash: { name: "SHA-256" },
-                },
-                true,
-                ["encrypt", "decrypt"]
-            ).then(function (keyPair) {
-                window.crypto.subtle.exportKey("jwk", keyPair.publicKey).then(function (publicKey) {
-                    generatedVaultKeys[m + "/" + y].publicKey = publicKey;
-                });
-                window.crypto.subtle.exportKey("jwk", keyPair.privateKey).then(function (privateKey) {
-                    generatedVaultKeys[m + "/" + y].privateKey = privateKey;
-                });
+        generatedVaultKeys[y] = {};
+        window.crypto.subtle.generateKey(
+            {
+                name: "RSA-OAEP",
+                modulusLength: 4096,
+                publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+                hash: { name: "SHA-256" },
+            },
+            true,
+            ["encrypt", "decrypt"]
+        ).then(function (keyPair) {
+            window.crypto.subtle.exportKey("jwk", keyPair.publicKey).then(function (publicKey) {
+                generatedVaultKeys[y].publicKey = publicKey;
+            });
+            window.crypto.subtle.exportKey("jwk", keyPair.privateKey).then(function (privateKey) {
+                generatedVaultKeys[y].privateKey = privateKey;
             });
         });
     });
